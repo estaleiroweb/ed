1feb675cc6ab42aca84ecd0e8155f55f238e6b76
estaleiroweb <helbert@estaleiroweb.com.br>
2022-04-25 16:00:05 -0300

    _::getTable

--- src/IO/_.php
+++ src/IO/_.php
@@ -240,6 +240,140 @@ class _ {
 		$result .= $fnFootLine($head['orgname'], true, $countRecords, $lines);
 		$fnCmd($result, $classStyle);
 	}
+	static public function getTable($arr, $showKey = '#', $maxLength = null, $countRecords = true, $classStyle = 'alert alert-primary MakeBox') {
+		if (!$arr) return;
+		$out = '';
+		if (is_array($arr)) {
+			$keys = array_keys(reset($arr));
+			$fn = function (&$arr) {
+				return $arr ? array_shift($arr) : false;
+			};
+		} elseif (is_object($arr) && ($arr instanceof Res || $arr instanceof PDOStatement)) {
+			$nCols = $arr->columnCount();
+			if (!$arr) return;
+			$keys = [];
+			for ($i = 0; $i < $nCols; $i++) $keys[] = $arr->getColumnMeta($i)['name'];
+			//foreach ($arr->fetch_fields() as $v) $keys[$v->name] = $v->name;
+			//print_r($keys);
+			$fn = function (&$arr) {
+				return $arr->fetch(PDO::FETCH_ASSOC);
+			};
+		} else return;
+		if ($showKey) array_unshift($keys, $showKey);
+		//print_r(array($fn,$pr));return;
+
+		if (@$_SERVER['SHELL']) {
+			if (is_null($maxLength)) $maxLength = 30;
+			$fnHeadTop = function ($arr, $flag, $countRecords, $lines) {
+				return self::fit_showHeadLineTop($arr, $flag);
+			};
+			$fnHead = function ($arr, $flag) {
+				return self::fit_Line($arr, $flag);
+			};
+			$fnHeadBottom = function ($arr, $flag) {
+				return self::fit_showHeadLineBottom($arr, $flag);
+			};
+			$fnField = function ($value, $length, $align) {
+				return self::fit_Field($value, $length, $align);
+			};
+			$fnLine = function ($line, $flag) {
+				return self::fit_Line($line, $flag);
+			};
+			$fnFootLine = function ($arr, $flag, $countRecords, $lines) {
+				$result = self::fit_showFootLine($arr, $flag);
+				if ($countRecords) $result .= count($lines) . " Records\n";
+				$result .= "\n";
+				return $result;
+			};
+			$fnCmd = function ($result, $classStyle) use ($out) {
+				$out .= $result;
+			};
+		} else {
+			if (is_null($maxLength)) $maxLength = 200;
+			new Ed();
+			$fnHeadTop = function ($arr, $flag, $countRecords, $lines) {
+				//table-bordered table-sm 
+				$result = '<div><div>[<a href="javascript:" onclick="$.copy(this.parentElement.nextSibling,\'html\')">Copy</a>]';
+				if ($countRecords) {
+					$recCount = count($lines);
+					$result .= "<b>$recCount Records</b>";
+				}
+				$result .= '</div>';
+				$result .= "<table class='table table-striped table-sm table-hover'>\n";
+				return $result;
+			};
+			$fnHead = function ($arr, $flag) {
+				$result = '<tr>';
+				foreach ($arr as $v) $result .= "<th>$v</th>";
+				$result .= "</tr>\n";
+				return $result;
+			};
+			$fnHeadBottom = function ($arr, $flag) {
+				return '';
+			};
+			$fnField = function ($value, $length, $align) {
+				if ($align == STR_PAD_LEFT) $class = 'text-end';
+				elseif ($align == STR_PAD_RIGHT) $class = 'text-start';
+				elseif ($align == STR_PAD_BOTH) $class = 'text-center';
+				else $class = '';
+				return '<td class="' . $class . '">' . htmlspecialchars($value) . '</td>';
+			};
+			$fnLine = function ($line, $flag) {
+				return '<tr>' . implode('', $line) . "</tr>\n";
+			};
+			$fnFootLine = function ($arr, $flag, $countRecords, $lines) {
+				return '</table></div>';
+			};
+			$fnCmd = function ($result, $classStyle) use ($out) {
+				$out .= $result;
+				//$out.=self::makeBox($result, '', $classStyle, 'pre');
+			};
+		}
+
+		$head = [];
+		foreach ($keys as $k) {
+			$head['length'][$k] = min(max(1, strlen($k)), $maxLength);
+			$head['align'][$k] = STR_PAD_LEFT;
+			$head['alignHead'][$k] = $showKey == $k ? STR_PAD_LEFT : STR_PAD_RIGHT;
+		}
+
+		$idx = 0;
+		$lines = [];
+		while ($line = $fn($arr)) {
+			//foreach($arr as $line) {
+			if ($showKey) $line[$showKey] = $idx++;
+			foreach ($head['length'] as $k => $len) {
+				$v = @$line[$k];
+				if (is_object($v)) $v = $line[$k] = ($v instanceof Field) ? $v() : json_encode($v);
+				elseif (is_array($v)) $v = $line[$k] = json_encode($v);
+				$head['length'][$k] = min(max($len, strlen($v)), $maxLength);
+				if ($v && !is_numeric($v)) $head['align'][$k] = STR_PAD_RIGHT;
+			}
+			$lines[] = $line;
+		}
+		foreach ($keys as $k) {
+			$head['orgname'][$k] = self::fit_Field($k, $head['length'][$k], @$head['alignHead'][$k]);
+			$head['line'][$k] = str_repeat('-', $head['length'][$k]);
+		}
+
+		$result = $fnHeadTop($head['orgname'], true, $countRecords, $lines);
+		$result .= $fnHead($head['orgname'], true);
+		$result .= $fnHeadBottom($head['orgname'], true);
+
+		foreach ($lines as $idx => $line) if (is_array($line)) {
+			if ($showKey) $line = array_merge([$showKey => $idx], $line);
+			foreach ($line as $k => $v) $line[$k] = $fnField($v, $head['length'][$k], $head['align'][$k]);
+			$result .= $fnLine($line, true);
+		} else {
+			$result .= print_r($line, true);
+			$result .= "\n";
+		}
+
+		$result .= $fnFootLine($head['orgname'], true, $countRecords, $lines);
+		$fnCmd($result, $classStyle);
+
+		return $out;
+	}
 
 	public function escape_string($str) {
 		$e = mb_detect_encoding($str);
